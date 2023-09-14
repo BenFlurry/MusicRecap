e503e4f6e30a3287370189a2823ab10e56ca3eb9
Ben Alexander <benjaminalexander747@gmail.com>
2023-09-14 23:07:50 +0100

    Initial Commit

---
+++ MusicRecap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicRecap
+//
+//  Created by Benjamin Alexander on 14/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicRecap/MusicRecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicRecapApp.swift
+//  MusicRecap
+//
+//  Created by Benjamin Alexander on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicRecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
