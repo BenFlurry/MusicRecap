bf318a12a26d03f5fca60fb9f8e57df90030e804
Ben Alexander <benjaminalexander747@gmail.com>
2023-09-15 11:44:12 +0100

    small start

---
+++ MusicRecap/SongView.swift
@@ -0,0 +1,18 @@
+//
+//  SongView.swift
+//  MusicRecap
+//
+//  Created by Benjamin Alexander on 14/09/2023.
+//
+
+import SwiftUI
+
+struct SongView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SongView()
+}
